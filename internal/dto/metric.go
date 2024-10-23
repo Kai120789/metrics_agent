4a3dfa828e033561f046cde1fa7d2612242d4b87
Kai120789 <zoomer120789@gmail.com>
2024-10-23 13:21:10 +0300

    feat: add struct tags to dto

--- internal/dto/metric.go
+++ internal/dto/metric.go
@@ -3,10 +3,10 @@ package dto
 import "time"
 
 type Metric struct {
-	ID        uint      // Уникальный идентификатор метрики
-	Name      string    // Название метрики
-	Type      string    // Тип метрики (counter или gauge)
-	Value     *float64  // Значение для метрик типа gauge
-	Delta     *int64    // Изменение для метрик типа counter
-	CreatedAt time.Time // Время создания метрики
+	ID        uint      `json:"id"`         // Уникальный идентификатор метрики
+	Name      string    `json:"name"`       // Название метрики
+	Type      string    `json:"type"`       // Тип метрики (counter или gauge)
+	Value     *float64  `json:"value"`      // Значение для метрик типа gauge
+	Delta     *int64    `json:"delta"`      // Изменение для метрик типа counter
+	CreatedAt time.Time `json:"created_at"` // Время создания метрики
 }
